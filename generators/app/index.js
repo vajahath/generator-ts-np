eeee42b917aa4f9091d51767851fe4cef292ee3c
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-07-19 19:14:39 +0530

    initial success

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,33 +4,74 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the pioneering ' + chalk.red('generator-ts-np') + ' generator!'
-    ));
+    prompting() {
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the pioneering ' + chalk.red('generator-ts-np') + ' generator!'
+        ));
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        const prompts = [{
+            type: 'input',
+            name: 'packageName',
+            message: 'Package Name',
+            default: this.appname
+        }, {
+            type: 'input',
+            name: 'packageDescription',
+            message: 'Package Description',
+        }, {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'Github Username',
+            store: true
+        }, {
+            type: 'input',
+            name: 'githubRepository',
+            message: 'Github Repository Name',
+            default: this.appname
+        }, {
+            type: 'input',
+            name: 'email',
+            message: 'Your Email',
+            store: true
+        }, {
+            type: 'input',
+            name: 'twitterUsername',
+            message: 'Twitter Username with @',
+            store: true
+        }];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
+        return this.prompt(prompts).then(props => {
+            // To access props later use this.props.someAnswer;
+            this.props = props;
+        });
+    }
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
+    writing() {
+        this.fs.copyTpl(
+            this.templatePath('**/*'),
+            this.destinationPath(), {
+                packageName: this.props.packageName,
+                packageDescription: this.props.packageDescription,
+                githubUsername: this.props.githubUsername,
+                githubRepository: this.props.githubRepository,
+                email: this.props.email,
+                twitterUsername: this.props.twitterUsername,
+            }
+        );
 
-  install() {
-    this.installDependencies();
-  }
+        this.fs.copy(
+            this.templatePath('.*'),
+            this.destinationPath()
+        );
+
+    }
+
+    install() {
+        this.installDependencies({
+            npm: false,
+            bower: false,
+            yarn: true
+        });
+    }
 };
