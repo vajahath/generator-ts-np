678e14ee9401b66f9719e110eeff0cb8b7b19818
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-07-27 10:39:15 +0530

    docs, version, bug

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # generator-ts-np
 Yeoman Generator For Building Node Packages With Typescript.
 
-> :warning: This package is in Beta. You are warned. 
+> :warning: This package is in Beta. You are warned!
 
 ![TS-NP generator](https://raw.githubusercontent.com/vajahath/generator-ts-np/master/media/logo.png)
+
+## Why?
+- Inspired from [Typescript's own guide for node](https://github.com/Microsoft/TypeScript-Node-Starter#typescript-node-starter).
+- Lint rules adopted from [Google APIs Node.js Client Repo](https://github.com/google/google-auth-library-nodejs) which ensures higher code quality.
+- Pre-commit hooks for linters.
+- Vscode integration.
+- Integrated, fully configured mocha testing environment.
+- Scaffolds readme with all essential badges and stuffs.
+- Familiar directory structure.
 
 ## Install
 ```
--- generators/app/index.js
+++ generators/app/index.js
@@ -4,80 +4,79 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-	prompting() {
-		// Have Yeoman greet the user.
-		this.log(yosay(
-			'Scaffold your next node package with ' + chalk.red('ts-np') + ' generator!'
-		));
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Scaffold your next node package with ' + chalk.red('ts-np') + ' generator!'
+    ));
 
-		const prompts = [{
-			type: 'input',
-			name: 'packageName',
-			message: 'Package Name',
-			default: this.appname
-		}, {
-			type: 'input',
-			name: 'packageDescription',
-			message: 'Package Description',
-		}, {
-			type: 'input',
-			name: 'githubUsername',
-			message: 'Github Username',
-			store: true
-		}, {
-			type: 'input',
-			name: 'githubRepository',
-			message: 'Github Repository Name',
-			default: this.appname
-		}, {
-			type: 'input',
-			name: 'email',
-			message: 'Your Email',
-			store: true
-		}, {
-			type: 'input',
-			name: 'twitterUsername',
-			message: 'Twitter Username without @',
-			store: true
-		}, {
-			type: 'input',
-			name: 'fullName',
-			message: 'Your full name to appear in README',
-			store: true
-		}];
+    const prompts = [{
+      type: 'input',
+      name: 'packageName',
+      message: 'Package Name',
+      default: this.appname
+    }, {
+      type: 'input',
+      name: 'packageDescription',
+      message: 'Package Description'
+    }, {
+      type: 'input',
+      name: 'githubUsername',
+      message: 'Github Username',
+      store: true
+    }, {
+      type: 'input',
+      name: 'githubRepository',
+      message: 'Github Repository Name',
+      default: this.appname
+    }, {
+      type: 'input',
+      name: 'email',
+      message: 'Your Email',
+      store: true
+    }, {
+      type: 'input',
+      name: 'twitterUsername',
+      message: 'Twitter Username without @',
+      store: true
+    }, {
+      type: 'input',
+      name: 'fullName',
+      message: 'Your full name to appear in README',
+      store: true
+    }];
 
-		return this.prompt(prompts).then(props => {
-			// To access props later use this.props.someAnswer;
-			this.props = props;
-		});
-	}
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
 
-	writing() {
-		this.fs.copyTpl(
-			this.templatePath('render/**'),
-			this.destinationPath(), {
-				packageName: this.props.packageName,
-				packageDescription: this.props.packageDescription,
-				githubUsername: this.props.githubUsername,
-				githubRepository: this.props.githubRepository,
-				email: this.props.email,
-				twitterUsername: this.props.twitterUsername,
-				fullName: this.props.fullName
-			}
-		);
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('render/**'),
+      this.destinationPath(), {
+        packageName: this.props.packageName,
+        packageDescription: this.props.packageDescription,
+        githubUsername: this.props.githubUsername,
+        githubRepository: this.props.githubRepository,
+        email: this.props.email,
+        twitterUsername: this.props.twitterUsername,
+        fullName: this.props.fullName
+      }
+    );
 
-		this.fs.copy(
-			this.templatePath('static/**'),
-			this.destinationPath(), { globOptions: { dot: true } }
-		);
+    this.fs.copy(
+      this.templatePath('static/**'),
+      this.destinationPath(), {globOptions: {dot: true}}
+    );
+  }
 
-	}
-
-	install() {
-		this.installDependencies({
-			npm: false,
-			bower: false,
-			yarn: true
-		});
-	}
+  install() {
+    this.installDependencies({
+      npm: false,
+      bower: false,
+      yarn: true
+    });
+  }
 };
