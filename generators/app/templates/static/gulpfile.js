104041940861bc5f5c7bea1a1f8fe7fdba3bc2b8
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-08-19 22:54:40 +0530

    `require`ing del addressing #18

--- generators/app/templates/static/gulpfile.js
+++ generators/app/templates/static/gulpfile.js
@@ -1,12 +1,13 @@
 "use strict";
 
 const gulp = require("gulp");
+const del = require('del');
 
 // copy json files from src to dist
 gulp.task("copy-json", () => {
-	return gulp.src("src/**/*.json").pipe(gulp.dest("dist/"));
+    return gulp.src("src/**/*.json").pipe(gulp.dest("dist/"));
 });
 
 gulp.task("clean-build", () => {
-	return del.sync(["dist/**/*", "dist/**/.*"]);
+    return del.sync(["dist/**/*", "dist/**/.*"]);
 });
