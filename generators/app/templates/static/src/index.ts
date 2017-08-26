11598e2c9e6a088b81cffd2c5c4ca7473e863fa7
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-08-26 13:40:10 +0530

    output code base linting

--- generators/app/templates/static/src/index.ts
+++ generators/app/templates/static/src/index.ts
@@ -1,3 +1,3 @@
-export default function(a:number, b:number){
-    return a+b;
+export default function(a: number, b: number) {
+	return a + b;
 }
--- generators/app/templates/static/tests/index.spec.ts
+++ generators/app/templates/static/tests/index.spec.ts
@@ -5,8 +5,8 @@ import add from '../src/index';
 
 describe('Testing addition', () => {
 	it('returns 11 for 3,8', done => {
-		let p = add(3, 8);
-		expect(p).to.equal(11)
+		const p = add(3, 8);
+		expect(p).to.equal(11);
 		done();
-	})
-})
+	});
+});
