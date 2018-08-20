d05116748e99fe12cbbe8e36bb14c076b7333d68
Vajahath Ahmed <vajuoff.1@gmail.com>
2018-08-20 08:12:56 +0530

    bug fixes

--- src/template/static/typings/typings.d.ts
+++ src/template/static/typings/typings.d.ts
@@ -4,17 +4,6 @@
  */
 
 declare module '*.json' {
-  const value: any;
-  export = value;
-}
-
-interface AsyncIterator<T> {
-  next(value?: any): Promise<IteratorResult<T>>;
-  return?(value?: any): Promise<IteratorResult<T>>;
-  throw?(e?: any): Promise<IteratorResult<T>>;
-}
-interface AsyncIterable<T> {
-  next(value?: any): Promise<IteratorResult<T>>;
-  return?(value?: any): Promise<IteratorResult<T>>;
-  throw?(e?: any): Promise<IteratorResult<T>>;
+  const val: any;
+  export = val;
 }
