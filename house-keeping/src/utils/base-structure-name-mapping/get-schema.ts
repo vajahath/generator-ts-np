a87a5e55f9f7133ba3e077e2ba0b9f02e421c915
Vajahath Ahmed <vajuoff.1@gmail.com>
2019-11-27 23:48:37 +0530

    fix(HK/get-schema): add support for windows

--- house-keeping/src/utils/base-structure-name-mapping/get-schema.ts
+++ house-keeping/src/utils/base-structure-name-mapping/get-schema.ts
@@ -1,14 +1,24 @@
 import { spawn } from 'child_process';
 import { join as pathJoin, resolve as pathResolve } from 'path';
 import { Writable } from 'stream';
+import isWindows = require('is-windows');
 
 const typeDefPathForSchema = pathJoin(
   __dirname,
   'base-structure-mapping.interface.ts'
 );
 
+// with cwd as root to HK dir
+const generatorExe = pathJoin(
+  'node_modules',
+  '.bin',
+  `ts-json-schema-generator${isWindows() ? '.cmd' : ''}`
+);
+
+const feedingCwd = pathResolve(__dirname, '..', '..', '..');
+
 export function getBaseStructureNamingJsonSchema(): Promise<object> {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const keepStore: Buffer[] = [];
     const keepWritable = new Writable({
       write(chunk, enc, callback) {
@@ -18,20 +28,23 @@ export function getBaseStructureNamingJsonSchema(): Promise<object> {
     });
 
     const schemaGeneratorProcess = spawn(
-      'node',
-      [
-        './node_modules/.bin/ts-json-schema-generator',
-        '--path',
-        typeDefPathForSchema
-      ],
+      generatorExe,
+      ['--path', typeDefPathForSchema],
       {
-        cwd: pathResolve(__dirname, '..', '..', '..')
+        cwd: feedingCwd
       }
     );
-    schemaGeneratorProcess.stdout.pipe(keepWritable).on('finish', () => {
-      const schemaAsString = Buffer.concat(keepStore).toString();
-      const json: object = JSON.parse(schemaAsString);
-      return resolve(json);
-    });
+    // schemaGeneratorProcess.stdout.pipe(process.stdout);
+    // schemaGeneratorProcess.stderr.pipe(process.stdout);
+    schemaGeneratorProcess.stdout
+      .pipe(keepWritable)
+      .on('finish', () => {
+        const schemaAsString = Buffer.concat(keepStore).toString();
+        const json: object = JSON.parse(schemaAsString);
+        return resolve(json);
+      })
+      .on('error', err => reject(err));
   });
 }
+
+// getBaseStructureNamingJsonSchema().then(obj => console.log(obj));
