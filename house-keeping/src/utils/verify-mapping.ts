a7abe3cff15039b12e449482c89c83e16a2bb9eb
Vajahath Ahmed <vajuoff.1@gmail.com>
2019-11-28 16:51:55 +0530

    feat(HK): add meta mapping verification function

--- house-keeping/src/utils/verify-mapping.ts
+++ house-keeping/src/utils/verify-mapping.ts
@@ -1,1 +1,20 @@
-export function verifyMappingFromBaseStructure(jsonFileContent: object) {}
+import Ajv = require('ajv');
+
+const ajv = new Ajv({ allErrors: true });
+
+export function verifyMappingFromBaseStructure(
+  schema: {
+    [key: string]: any;
+  },
+  jsonFileContent: {
+    [key: string]: any;
+  }
+) {
+  const isValidJson = ajv.validate(schema, jsonFileContent);
+
+  if (!isValidJson) {
+    throw new Error('The meta repo info is not correct.');
+  }
+
+  return isValidJson;
+}
---
+++ house-keeping/tests/utils/verify-mapping.spec.ts
@@ -0,0 +1,74 @@
+import { verifyMappingFromBaseStructure } from '../../dist/utils/verify-mapping';
+
+const schema = {
+  $ref: '#/definitions/IBaseStructureMapping',
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  definitions: {
+    IBaseStructureMapping: {
+      additionalProperties: {
+        additionalProperties: false,
+        properties: {
+          default: {},
+          description: { type: 'string' },
+          key: { type: 'string' },
+          name: { type: 'string' }
+        },
+        required: ['key', 'name', 'description', 'default'],
+        type: 'object'
+      },
+      type: 'object'
+    }
+  }
+};
+
+describe('testing meta-json validator', () => {
+  test('testing with good data', () => {
+    const data = {
+      npmScope: {
+        name: 'NPM scope',
+        key: '__tsnp__npm_scope',
+        description: 'npm scope with @',
+        default: null
+      }
+    };
+
+    expect(verifyMappingFromBaseStructure(schema, data)).toBe(true);
+  });
+  test('testing with bad data - 1', () => {
+    const data = {
+      npmScope: {
+        // name: 'NPM scope',
+        key: '__tsnp__npm_scope',
+        description: 'npm scope with @',
+        default: null
+      }
+    };
+
+    expect(() => verifyMappingFromBaseStructure(schema, data)).toThrow();
+  });
+  test('testing with bad data - 2', () => {
+    const data = {
+      npmScope: {
+        name: 'NPM scope',
+        key: '__tsnp__npm_scope',
+        // description: 'npm scope with @',
+        default: null
+      }
+    };
+
+    expect(() => verifyMappingFromBaseStructure(schema, data)).toThrow();
+  });
+  test('testing with bad data - 3', () => {
+    const data = {
+      npmScope: {
+        name: 'NPM scope',
+        key: '__tsnp__npm_scope',
+        description: 'npm scope with @',
+        descriptionExtra: 'npm scope with @',
+        default: null
+      }
+    };
+
+    expect(() => verifyMappingFromBaseStructure(schema, data)).toThrow();
+  });
+});
