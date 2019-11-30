edc2adf5f6b582760a265b39cf2fe4eddffa2fb5
Vajahath Ahmed <vajuoff.1@gmail.com>
2019-11-30 23:10:32 +0530

    feat(HK): remove older schema verification mechanism

--- house-keeping/src/utils/base-structure-name-mapping/index.ts
+++
@@ -1,4 +0,0 @@
-import { getBaseStructureNamingJsonSchema } from './get-base-structure-naming-schema';
-import { IBaseStructureMapping } from './base-structure-mapping.interface';
-
-export { getBaseStructureNamingJsonSchema, IBaseStructureMapping };
