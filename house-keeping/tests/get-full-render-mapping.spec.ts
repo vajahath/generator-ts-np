1768cc42180dfd3f8f5d08e2a40df226350b8848
Vajahath Ahmed <vajuoff.1@gmail.com>
2019-11-30 23:13:02 +0530

    test(HK): add tests for new HK mechanisms

---
+++ house-keeping/tests/get-full-render-mapping.spec.ts
@@ -0,0 +1,30 @@
+import { getFullTSNPPrompts } from '../dist/mappings/get-full-prompts';
+
+import { GetRenderPromptVariablesResult } from '../dist/mappings/Types';
+
+describe('testing get for full tsnp prompts', () => {
+  test('testing with mocked yeoman generator context', () => {
+    let result: GetRenderPromptVariablesResult = {
+      queries: [],
+      scopedPackageNameKey: 'x'
+    };
+
+    {
+      class Mock {
+        public appname = 'appname';
+        public user = {
+          github: { username: 'github-username' },
+          git: { name: 'git-name', email: 'git-email' }
+        };
+
+        constructor() {
+          result = getFullTSNPPrompts.call(this as any);
+        }
+      }
+      new Mock();
+    }
+
+    expect(result.queries.length).not.toBe(0);
+    expect(result.scopedPackageNameKey).not.toBe('x');
+  });
+});
